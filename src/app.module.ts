09dfead8299cd4d71ca721902e08aee5a592019a
hyuk-dev <hyuk.development@gmail.com>
2025-11-17 13:25:29 +0900

    Normalize line endings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -20,13 +21,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: false,
+        autoLoadEntities: true,
+        synchronize: true,
         ssl: {
           rejectUnauthorized: false,
         },
       }),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
